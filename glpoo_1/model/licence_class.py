7020b6e83517959e766c6ab604c1a8610eeca6f0
Maxime-Dewit <dewit.maximes@gmail.com>
2021-04-17 14:27:23 +0200

    licence_class

---
+++ glpoo_1/model/licence_class.py
@@ -0,0 +1,32 @@
+from model import *
+
+
+class Licence:
+
+    def __init__(self, id, id_club, name, prix, nb_sceance, avantage):
+        self.id = id
+        self.id_club = id_club
+        self.name = name
+        self.prix = prix
+        self.nb_sceance = nb_sceance
+        self.avantage = avantage
+
+    def modifier_licence(self, name=None, prix=None, nb_sceance=None, avantage=None):
+        if name:
+            self.name = name
+        if prix:
+            self.prix = prix
+        if nb_sceance:
+            self.nb_sceance = nb_sceance
+        if avantage:
+            self.avantage = avantage
+        modify_licence(self.id, name=name, prix=prix, nb_sceance=nb_sceance, avantage=avantage)
+
+    def supprimer_license(self):
+        del_licence(id)
+
+
+def creer_License(id_club, name, prix, nb_sceance, avantage):
+    new_licence = Licence()
+    new_licence.init(id_club, name, prix, nb_sceance, avantage)
+    add_licence(id_club, name, prix, nb_sceance, avantage)
