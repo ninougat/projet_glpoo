19b7d591830822b03c90af1ee1dcfbdca4331878
Vincent <catherine@et.esiea.fr>
2021-04-27 15:35:32 +0200

    ajout des clubs et evenements dans la BDD + correction de quelques petites choses

--- glpoo_1/model/club_class.py
+++ glpoo_1/model/club_class.py
@@ -13,18 +13,21 @@ class Club:
 
     def changer_nom(self, nom):
         self.nom = nom
+        modify_club(id, nom=nom)
 
     def afficher_adresse(self):
         print(self.adresse)
 
     def changer_adresse(self, adresse):
         self.adresse = adresse
+        modify_club(id, adresse=adresse)
 
     def afficher_description(self):
         print(self.description)
 
     def changer_description(self):
         self.description = input("Entrez la nouvelle description")
+        modify_club(id, description=self.description)
 
     def modifier_logo(self, logo):
         self.logo = logo
@@ -44,7 +47,7 @@ class Club:
 
 
     def afficher_membres(self):
-        licenses=session.query(Licence_bdd).filter_by(id=self.id)#on récupère toute les licences du clubs
+        licenses=session.query(Licence_bdd).filter_by(id=self.id)#on récupère toute les licences du club
         for i in licenses : #pour chaque licence du club
             mem_lic=session.query(Member_licence).filter_by(id_licence=i.id)#on récupère la table intermédiaire entre membre et licence
             for j in mem_lic :#pour chaque ligne de la table
@@ -55,22 +58,26 @@ class Club:
     def ajouter_membre(self, membre):
         self.membres.append(membre)
 
+
     def supprimer_membre(self, membre):
         for i in range(len(self.membres)):
             if self.membres[i] == membre:
                 self.membres.pop(i)
+        session.delete(session.query(Member_licence).filter_by(user=id).one()) # on supprime la liaison entre l'utilisateur et la license dans la base de données
+        session.commit()
 
     def afficher_evenements(self):
         for evenement in self.calendrier_evenements:
             evenement.afficher_evenement()
 
-    def ajouter_evenement(self, nom, lieu, date, horraire):
-        evenement = Evenement(nom, lieu, date, horraire)
+    def ajouter_evenement(self, nom, lieu, date, horaire):
+        evenement = Evenement(nom, lieu, date, horaire)
         print("Entrez la description de l'événement :")
         evenement.nom = input("Nom :")
         evenement.lieu = input("Lieu :")
         evenement.date = input("Date (jj/mm/aaaa) :")
         evenement.horraire = input("Horraire (HHhMM) :")
         self.calendrier_evenements.append(evenement)
-        #TODO ajout BDD
+        # on ajoute l'évènement dans la base de données
+        add_event(id, evenement)
         print(f"L'énévement {evenement.nom} a bien été ajouté au calendrier.")
