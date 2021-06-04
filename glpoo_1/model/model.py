27be5a1d22d4e3428feb3b6f9bddd10aad818fe8
theod <theodute@gmail.com>
2021-06-04 15:48:56 +0200

    correction des petites erreurs

--- glpoo_1/model/model.py
+++ glpoo_1/model/model.py
@@ -130,15 +130,18 @@ def add_member_licence(id_member,id_licence,statut):
 
 def modify_membre_licence(ida=None,id_licence=None,statut=None,id_member=None):
      try:
+         mod=None
          if id_licence and id_member:
             mod=session.query(Member_licence).filter_by(id_licence=id_licence,id_member=id_member).one()
          elif ida:
             mod= session.query(Member_licence).filter_by(id=ida).one()
-
-         if id_licence :
-             mod.id_licence=id_licence
-         if statut :
-             mod.statut=statut
+         if mod:
+             if id_licence :
+                 mod.id_licence=id_licence
+             if statut :
+                 mod.statut=statut
+         else :
+             print("cette colonne n'existe pas")
      except :
          print("cette colonne n'existe pas")
 
@@ -188,12 +191,12 @@ def del_licence(ida):
 
 
 def get_club_by_licence(id_licence):
-    licence=session.query(Licence_bdd).filter_by(id=ida).one()
+    licence=session.query(Licence_bdd).filter_by(id=id_licence).one()
     if licence:
         club=session.query(Club_bdd).filter_by(id=licence.id_club).one()
         if club :
             return club
-    return none
+    return None
 
 def list_licences_by_club(id_club):
     licence=session.query(Licence_bdd).filter_by(id_club=id_club)
@@ -201,12 +204,12 @@ def list_licences_by_club(id_club):
 
 
 def list_members_by_club(id_club):
-    licences=list_licence_by_club(id_club)
+    licences=list_licences_by_club(id_club)
     members=[]
     for licence in licences :
         members_by_licence=list_members_by_licence(licence.id)
         for member in members_by_licence :
-            members.push(member)
+            members.append(member)
     return members
 
 
