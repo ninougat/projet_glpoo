b6abc704bb095119abc9ec7de490d1bc35cba8c8
theod <theodute@gmail.com>
2021-06-04 19:34:23 +0200

    ajout un membre dans un club

--- glpoo_1/model/model.py
+++ glpoo_1/model/model.py
@@ -257,9 +257,10 @@ def list_licences_by_club(id_club):
 
 def create_club(club,licence):
     add_club(club)
+    print(club.id + "le club qui fonctionne")
     licence.definir_id_club(club.id)
     add_licence(licence)
-    print(licence.id+ "la licence qui marcge")
+    print(licence.id +"la licence qui marche")
     add_member_licence(club.chef,licence.id,2)
 
 
