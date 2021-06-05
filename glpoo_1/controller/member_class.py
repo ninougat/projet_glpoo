3589cab6dbdd9c82df556df917cd45585eaceb0d
Vincent <catherine@et.esiea.fr>
2021-06-05 15:50:54 +0200

    correction connexion club pour le cas où on n'est pas inscrit dans le club

--- glpoo_1/controller/member_class.py
+++ glpoo_1/controller/member_class.py
@@ -109,6 +109,8 @@ def nouveau_membre(nom, prenom, pseudo, mot_de_passe):
 
 def connexion_club(user, club):
     licence_bdd, statut = get_licence_by_club_and_member(user.id, club.id)
+    if licence_bdd is None or statut is None:
+        return None
     membre = Membre(user.name, user.firstname, user.user, user.password, statut, licence_bdd.id)
     membre.clubs = user.clubs.copy()
     return membre
